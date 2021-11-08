080b593a534c7cbcb45e72e1ad1f3a9b721d257b
abhishekpachkawade <abhishekpachkawade@gmail.com>
2021-11-08 07:38:51 +0530

    Header and Footer

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HeaderComponent } from './Components/header/header.component';
+
+const routes: Routes = [
+  {
+    path:'header',component:HeaderComponent
+  },
+  {
+    path:'Footer',component:FooterComponent
+  },
+  {
+    path:'main',component:FooterComponent
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
